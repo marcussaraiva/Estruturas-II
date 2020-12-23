8f91fdbfe120e20456e7512a751b38dc21b5bcac
Marcus Saraiva <55753204+marcussaraiva@users.noreply.github.com>
2020-12-23 14:08:04 -0300

    Implementação Deque

---
+++ Listas/deque.py
@@ -0,0 +1,36 @@
+class Deque:
+
+        def __init__(self, tamanho):
+            self.tamanho = tamanho
+            self.inicio = None
+            self.fim = None
+            self.deque = []
+        
+        def inserir_inicio(self,elemento):
+            if(self.tamanho == len(self.deque)):
+                print("Deque cheio")
+            else: return self.deque.insert(0,elemento)
+
+        def inserir_final(self,elemento):
+            if(self.tamanho == len(self.deque)):
+                print("Deque cheio")
+            else: return self.deque.append(elemento)
+
+        def remover_inicio(self):
+            if(len(self.deque) > 0):
+                elemento_removido = self.deque[0]
+                del self.deque[0]
+                return elemento_removido
+            return False
+
+        def remover_final(self):
+            if(len(self.deque) > 0):
+                return self.deque.pop()
+            return False
+
+        def mostrar(self):
+            if(len(self.deque) > 0):
+                print(str(self.deque))
+            elif(len(self.deque) == 0):
+                print("Fila Vazia!")
+        
---
+++ Listas/testeDeque.py
@@ -0,0 +1,15 @@
+from deque import *
+
+def main():
+    deque = Deque(5)
+    deque.inserir_inicio("Pri")
+    deque.inserir_inicio("Seg")
+    deque.inserir_inicio("Ter")
+    deque.inserir_final("Normal")
+    deque.mostrar()
+    deque.remover_inicio()
+    deque.remover_final()
+    deque.mostrar()
+
+if __name__ == "__main__":
+    main()
